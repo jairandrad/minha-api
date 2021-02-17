d4ca0233c2e0e66c0b9b782a8e921d6d5ebeb6b4
Jair Andrade <jair.andrade4@etec.sp.gov.br>
2021-02-16 21:54:50 -0300

    Retornando JSON usando método GET

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Minha API'));
-
+app.get('/', (req, res) => {
+    const result = {name: 'jair', email: 'jair.andrad.filho@gmail.com'};
+    return res.json(result);
+});
 app.listen(port, () => console.log(`Listening on port ${port}!`));
