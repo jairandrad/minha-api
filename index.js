1bdbc38583b45caa0764d723db61e4af29f97087
Jair Andrade <jair.andrade4@etec.sp.gov.br>
2021-02-16 22:19:30 -0300

    Adicionado body-parser utilizando método urlencoded

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     const result = {name: 'jair', email: 'jair.andrad.filho@gmail.com'};
@@ -8,7 +12,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    return res.json([]);
+    const name = req.body.name;
+    const email = req.body.email;
+    return res.json([email, name]);
 });
 
 
