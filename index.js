7a82e43cfbe3708419a4830b61420dd6e4e5d2cd
Jair Andrade <jair.andrade4@etec.sp.gov.br>
2021-02-16 22:36:00 -0300

    Adicionado array com dados

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const data = [
+    { id: 1, name: 'Mike', age: 22, company: 'Nascetur Mus Company' },
+    { id: 2, name: 'Eleanor', age: 42, company: 'Hendrerit Donec LLP' },
+    { id: 3, name: 'Dylan', age: 51, company: 'Nisi Incorporated' },
+    { id: 4, name: 'Leila', age: 30, company: 'Eros Non Limited' },
+    { id: 5, name: 'Jason', age: 31, company: 'Accumsan Interdum Associates' },
+  ];
 
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    const result = {name: 'jair', email: 'jair.andrad.filho@gmail.com'};
-    return res.json(result);
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const item = data.find(item => item.id == id);
+    return res.json(item);
 });
 
 app.post('/', (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     return res.json([email, name]);
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const item = data.find(item => item.id == id);
+    return res.json(item);
 });
 
 
